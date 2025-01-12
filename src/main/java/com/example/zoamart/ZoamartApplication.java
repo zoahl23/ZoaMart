b1f0ff4659bcafd0bafcbc7462147106fbd325cf
zoahl23 <hlan22102003@gmail.com>
2025-01-12 10:11:51 +0700

    init project

---
+++ src/main/java/com/example/zoamart/ZoamartApplication.java
@@ -0,0 +1,13 @@
+package com.example.zoamart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoamartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoamartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zoamart/ZoamartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zoamart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoamartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
