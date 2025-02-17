8104feb91e4c9e074b5ef69c5f4fda09d333a1fa
zoahl23 <hlan22102003@gmail.com>
2025-02-17 14:49:28 +0700

    temp: disable spring security

--- src/main/java/com/example/zoamart/ZoamartApplication.java
+++ src/main/java/com/example/zoamart/ZoamartApplication.java
@@ -3,7 +3,9 @@ package com.example.zoamart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+// tạm thời tắt Security để mỗi lần chạy ko phải login
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class ZoamartApplication {
 
 	public static void main(String[] args) {
