0fa2d0e294e2a6fe6a7017bf8c29d68cb5b6030e
zoahl23 <hlan22102003@gmail.com>
2025-02-19 14:53:57 +0700

    feat: send data from view to controller

--- src/main/java/com/example/zoamart/controller/UserController.java
+++ src/main/java/com/example/zoamart/controller/UserController.java
@@ -2,8 +2,11 @@ package com.example.zoamart.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.zoamart.domain.User;
 import com.example.zoamart.service.UserService;
 
 @Controller
@@ -20,6 +23,18 @@ public class UserController {
         String test = this.userService.handleHello();
         model.addAttribute("keyhihi", test);
         model.addAttribute("nameKey", "gia tri");
+        return "index";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User u) {
+        System.out.println("helo" + u);
         return "index";
     }
 
