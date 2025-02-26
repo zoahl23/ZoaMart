80b5af39ef8b8d5dac5f82728858ca950fca48ab
zoahl23 <hlan22102003@gmail.com>
2025-02-26 14:32:34 +0700

    feat: validate form create user

--- src/main/java/com/example/zoamart/controller/admin/ProductController.java
+++ src/main/java/com/example/zoamart/controller/admin/ProductController.java
@@ -1,14 +1,23 @@
 package com.example.zoamart.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.zoamart.domain.Product;
 
 @Controller
 public class ProductController {
 
     @GetMapping("/admin/product")
-    public String getDashboard() {
+    public String getProduct() {
         return "admin/product/show"; // show.jsp
+    }
+
+    @GetMapping("/admin/product/create")
+    public String getCreateProductPage(Model model) {
+        model.addAttribute("newProduct", new Product());
+        return "admin/product/create";
     }
 
 }
--- src/main/java/com/example/zoamart/domain/User.java
+++ src/main/java/com/example/zoamart/domain/User.java
@@ -11,6 +11,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "users")
@@ -18,9 +21,19 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // ID tự động tăng
     private long id;
+
+    @NotNull
+    @Email
     private String email;
+
+    @NotNull
+    @Min(2)
     private String password;
+
+    @NotNull
+    @Min(2)
     private String fullName;
+
     private String address;
     private String phone;
     private LocalDateTime createdAt;
