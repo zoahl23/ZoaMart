9e4a3d9d41eb305a385fc76bde5d013dac0fbc06
zoahl23 <hlan22102003@gmail.com>
2025-03-01 17:56:33 +0700

    feat: table category

--- src/main/java/com/example/zoamart/controller/admin/CategoryController.java
+++ src/main/java/com/example/zoamart/controller/admin/CategoryController.java
@@ -1,13 +1,27 @@
 package com.example.zoamart.controller.admin;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.zoamart.domain.Category;
+import com.example.zoamart.service.CategoryService;
 
 @Controller
 public class CategoryController {
 
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping("/admin/category")
-    public String getDashboard() {
+    public String getDashboard(Model model) {
+        List<Category> categories = this.categoryService.getAllCategories();
+        model.addAttribute("categories", categories);
         return "admin/category/show"; // show.jsp
     }
 
--- src/main/java/com/example/zoamart/service/CategoryService.java
+++ src/main/java/com/example/zoamart/service/CategoryService.java
@@ -1,8 +1,22 @@
 package com.example.zoamart.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.example.zoamart.domain.Category;
+import com.example.zoamart.repository.CategoryRepository;
 
 @Service
 public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return this.categoryRepository.findAll();
+    }
 
 }
