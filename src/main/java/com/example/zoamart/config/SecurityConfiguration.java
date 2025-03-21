117609bf5d847619188c30202bc005e1416c6532
zoahl23 <hlan22102003@gmail.com>
2025-03-21 12:41:10 +0700

    feat: redirect admin to admin page

--- src/main/java/com/example/zoamart/config/SecurityConfiguration.java
+++ src/main/java/com/example/zoamart/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import com.example.zoamart.service.CustomUserDetailsService;
 import com.example.zoamart.service.UserService;
@@ -43,6 +44,11 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    public AuthenticationSuccessHandler customSuccessHandler() {
+        return new CustomSuccessHandler();
+    }
+
+    @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
@@ -61,6 +67,7 @@ public class SecurityConfiguration {
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
                         .failureUrl("/login?error")
+                        .successHandler(customSuccessHandler())
                         .permitAll());
 
         return http.build();
